ba2284d6295bac06cb367592d8be7963a7e296b4
Fede G <fedeguzman07@gmail.com>
2018-10-30 13:01:41 -0300

    Otro reporte

---
+++ Hoyts/Forms/Reportes/ReporteHistoricoDePrecios/WinFormReporteHistoricoDePrecios.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hoyts.Forms.Reportes.ReporteHistoricoDePrecios
+{
+    public partial class WinFormReporteHistoricoDePrecios : Form
+    {
+        public WinFormReporteHistoricoDePrecios()
+        {
+            InitializeComponent();
+        }
+
+        private void WinFormReporteHistoricoDePrecios_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'ReporteHistoricoDePrecios.Precio' Puede moverla o quitarla según sea necesario.
+            this.PrecioTableAdapter.Fill(this.ReporteHistoricoDePrecios.Precio);
+
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void btn_cargar_Click(object sender, EventArgs e)
+        {
+            this.PrecioTableAdapter.Fill(this.ReporteHistoricoDePrecios.Precio);
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
--- Hoyts/Forms/ReportesMain.Designer.cs
+++ Hoyts/Forms/ReportesMain.Designer.cs
@@ -154,6 +154,7 @@
             this.button5.TabIndex = 26;
             this.button5.Text = "Reporte de Precios Actuales";
             this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // ReportesMain
             // 
